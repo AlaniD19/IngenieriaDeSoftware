b0f1ca79f2b6e81261f1992bfdf99bd6a1fd958f
Alan Delgado <alan_id_19@hotmail.com>
2021-12-08 21:07:11 -0600

    Update - LOgin system / Dashboard template / Quiz game

--- src/index.js
+++ src/index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const {database} = require('./keys');
 
 // initialization
 const app = express();
+require('./lib/passport');
+
 
 // settings
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname,'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use(session({
+    secret: 'mathebasics',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+  }));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
 // global variables
 app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
+    app.locals.message = req.flash('message');
     next();
 });
 
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
-module.exports = router;
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/',
+    failureFlash: true
+}));
+
+router.post('/signin', (req, res, next) => {
+    passport.authenticate('local.signin', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/',
+        failureFlash: true
+    })(req, res, next)
+});
+
+router.get('/dashboard/profile', (req, res) => {
+    res.render('dashboard.html');
+});
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard.html');
+});
+
+module.exports = router;  
